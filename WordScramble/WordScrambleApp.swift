ad9f74da236a360536d1423500bf90256fe6cddb
vgkon <vgkonstantakopoulos@gmail.com>
2022-11-08 19:35:19 +0200

    last edited summer '22

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by v.konstantakopoulos on 20/7/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
